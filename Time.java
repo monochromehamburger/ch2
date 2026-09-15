c757d9c0c593c1fe6aa116ec465e2910549e7f83
monochromehamburger <2598787@learn.vsb.bc.ca>
2026-09-15 15:01:05 -0700

    Add files via upload

--- Time.java
+++ Time.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class Time{
 	public static void main(String[] args){
 		int hour=12;
@@ -6,5 +5,11 @@ public class Time{
 		int second=14;
 		System.out.println("Seconds since midnight: "+ (hour*3600+minute*60+second));
 		System.out.println("Percent of day: "+ ((hour*3600.0+minute*60.0+second)/(24.0*3600)*100)+"%");
+		
+		int hour2=14;
+		int minute2=9;
+		int second2=34;
+		System.out.println("Seconds since assignment started: "+ ((hour2-hour)*3600+(minute2-minute)*60+second2-second));
+		
 	}
 }
